c79518f0b04565022919c1ba1f23cb4f1cf6cfd8
deadpool2022 <vikasgl2021@outlook.com>
2022-01-09 09:24:16 +0530

    added tailwindcss and course details setion

---
+++ components/CourseSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CourseSection({details}) {
+    console.log("🚀 ~ file: CourseSection.js ~ line 4 ~ CourseSection ~ details", details)
+    return (
+        <div>
+            <h1>Course Section</h1>
+        </div>
+    )
+}
